9670e69ee720e268b11b69c9f323266222161d7b
Ricardo Anastácio <ricardoanastacio123@gmail.com>
2020-03-06 00:12:04 +0000

    olaaa

--- gitScrpit.sh
+++ gitScrpit.sh
@@ -1,7 +1,8 @@
 if [ "$1" != "" ]; then
-    echo "Positional parameter 1 contains something"
+	git add -A && git commit -m "$1"
+	git push
+	echo "End"
 else
     echo "Positional parameter 1 is empty"
+	echo "Put commit Menssage arg"
 fi
-git add -A && git commit -m "$1"
-git push
