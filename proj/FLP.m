cffe0858d62a24e5d1f8aaa49cae8e384144d3f5
Ricardo Anastácio <ricardoanastacio123@gmail.com>
2020-03-20 17:43:40 +0000

    COMMIT

--- proj/FLP.m
+++ proj/FLP.m
@@ -1,18 +1,18 @@
+% 
+% % FIR least-squares Lowpass filter designed using the FIRLS function.
+% % All frequency values are in Hz.
+% Fs = 48000;  % Sampling Frequency
+% 
+% N     = 7;     % Order
+% Fpass = 0.02*Fs;   % Passband Frequency
+% Fstop = 0.05*Fs;  % Stopband Frequency
+% Wpass = 1;     % Passband Weight
+% Wstop = 0.3;   % Stopband Weight
+% 
+% % Calculate the coefficients using the FIRLS function.
+% b  = firls(N, [0 Fpass Fstop Fs/2]/(Fs/2), [1 1 0 0], [Wpass Wstop]);
+% Hd = dfilt.dffir(b);
+% freqz(Hd);
+% 
 
-% FIR least-squares Lowpass filter designed using the FIRLS function.
-% All frequency values are in Hz.
-Fs = 48000;  % Sampling Frequency
-
-N     = 7;     % Order
-Fpass = 0.02*Fs;   % Passband Frequency
-Fstop = 0.05*Fs;  % Stopband Frequency
-Wpass = 1;     % Passband Weight
-Wstop = 0.3;   % Stopband Weight
-
-% Calculate the coefficients using the FIRLS function.
-b  = firls(N, [0 Fpass Fstop Fs/2]/(Fs/2), [1 1 0 0], [Wpass Wstop]);
-Hd = dfilt.dffir(b);
-freqz(Hd);
-
-
-% [EOF]
+freqz([16,22,28,30,30,28,22,16])
